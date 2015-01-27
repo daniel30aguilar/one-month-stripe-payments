23c200d1cb3888cec1f5d9c6bf256d2f186aa09b
Daniel Aguilar <aguilar_daniel@hotmail.com>
2015-01-27 12:48:44 -0500

    added the ability to store purchases and display your purchase

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -14,6 +14,12 @@ class ChargesController < ApplicationController
 	    :currency    => 'usd'
 	  )
 
+	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+    	amount: params[:amount], description: charge.description, currency: charge.currency,
+    	customer_id: customer.id, product_id: 1)
+
+	  redirect_to purchase
+
 	rescue Stripe::CardError => e
 	  flash[:error] = e.message
 	  redirect_to charges_path
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # Support stripe payments through charges
   resources :charges
-  
+
+  # Route for showing purchases
+  resources :purchases, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
