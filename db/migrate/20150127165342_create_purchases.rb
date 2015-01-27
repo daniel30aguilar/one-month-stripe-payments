db4e3cab5f022b047f7aec0e21258a8188cc5f20
Daniel Aguilar <aguilar_daniel@hotmail.com>
2015-01-27 12:13:33 -0500

    Add purchases model and created database table for it

---
+++ db/migrate/20150127165342_create_purchases.rb
@@ -0,0 +1,15 @@
+class CreatePurchases < ActiveRecord::Migration
+  def change
+    create_table :purchases do |t|
+
+	    t.string :email
+	    t.integer :amount
+	    t.string :description
+	    t.string :currency
+	    t.string :customer_id
+	    t.string :card
+	    t.integer :product_id
+	    t.timestamps      
+    end
+  end
+end
