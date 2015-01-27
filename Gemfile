5efeca706082b3148f88a3c25c3b18be8ecc8eeb
Daniel Aguilar <aguilar_daniel@hotmail.com>
2015-01-27 16:34:26 -0500

    Implemented gemfiles for handling emailin in both development and production

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem  'stripe', :git =>  'https://github.com/stripe/stripe-ruby'
 # Figaro gem for securely managin credentials
 gem 'figaro'
 
+# Add letteropener for working with mail in dev mode
+gem 'letter_opener', group: :development 
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Send emails using letter opener to open the emails in a browser
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
