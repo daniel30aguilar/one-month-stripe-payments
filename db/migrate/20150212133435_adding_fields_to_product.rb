a0467f9f7087dfdce951ab6820d9647f76a5f625
Daniel Aguilar <aguilar_daniel@hotmail.com>
2015-02-12 16:15:22 -0500

    deleting migrations

--- db/migrate/20150212133435_adding_fields_to_product.rb
+++
@@ -1,7 +0,0 @@
-class AddingFieldsToProduct < ActiveRecord::Migration
-  def change
-  	add_column :products, :length, :string
-		add_column :products, :author_description, :text
-		add_column :products, :author_image_name, :string
-  end
-end
